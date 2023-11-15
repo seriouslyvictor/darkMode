53b09b72ad7c59f364c9f50da72d24681fde24a2
seriouslyvictor <schiavovictor@outlook.com>
2023-11-15 11:29:15 -0300

    added timelogic to autochange the theme

--- darkmode.js
+++ darkmode.js
@@ -1,6 +1,8 @@
 // const toggle =  document.querySelector("button");
 const toggle = document.querySelector("img");
 
+
+
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("tema--escuro");
   toggle.src =
@@ -8,3 +10,19 @@ toggle.addEventListener("click", () => {
       ? "icon-sun.svg"
       : "icon-moon.svg";
 });
+
+// Togglear automaticamente conforme horário local do usuário
+
+let hora = new Date().getHours();
+
+atualizarTema();
+function atualizarTema() {
+// verificar se é de "noite", caso seja, colocamos o tema escuro, caso não seja, REMOVEMOS o tema escuro
+    hora >= 18 || hora <= 6
+    ? document.body.classList.add("tema--escuro")
+    : document.body.classList.remove("tema--escuro");
+}
+
+// 
+
+setInterval(atualizarTema, 60000)
