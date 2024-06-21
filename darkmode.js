bca80a2b2f8fb5576bf0ef4f9ad30afccf65a47d
seriouslyvictor <schiavovictor@outlook.com>
2024-06-21 16:53:38 -0300

    Adicionado opção de salvar o tema com localStorage

--- darkmode.js
+++ darkmode.js
@@ -1,7 +1,24 @@
 const switcheroo = document.querySelector("#theme--toggle");
+const preferencia = localStorage.getItem("theme") || "light";
 
-switcheroo.addEventListener("change", () => {
-  const elBody = document.body;
-  elBody.classList.toggle("dark");
-  elBody.classList.toggle("light");
-});
+switcheroo.addEventListener("change", alterarTema);
+
+function alterarTema() {
+  const temaAtual = document.body.getAttribute("class");
+  const body = document.body;
+  if (temaAtual === "light") {
+    body.classList.add("dark");
+    body.classList.remove("light");
+    localStorage.setItem("theme", "dark");
+  } else {
+    body.classList.remove("dark");
+    body.classList.add("light");
+    localStorage.setItem("theme", "light");
+  }
+}
+
+function resgatarTema() {
+  document.body.classList.add(preferencia);
+}
+
+resgatarTema();
