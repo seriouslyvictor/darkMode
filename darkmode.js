8668d8a0a3a010763e65a11dabd51c45d69db622
seriouslyvictor <schiavovictor@outlook.com>
2024-06-21 16:39:31 -0300

    refatorado todo código, js reduzido para 7 linhas

--- darkmode.js
+++ darkmode.js
@@ -1,28 +1,7 @@
-// const toggle =  document.querySelector("button");
-const toggle = document.querySelector("img");
+const switcheroo = document.querySelector("#theme--toggle");
 
-
-
-toggle.addEventListener("click", () => {
-  document.body.classList.toggle("tema--escuro");
-  toggle.src =
-    toggle.getAttribute("src") === "icon-moon.svg"
-      ? "icon-sun.svg"
-      : "icon-moon.svg";
+switcheroo.addEventListener("change", () => {
+  const elBody = document.body;
+  elBody.classList.toggle("dark");
+  elBody.classList.toggle("light");
 });
-
-// Togglear automaticamente conforme horário local do usuário
-
-let hora = new Date().getHours();
-
-atualizarTema();
-function atualizarTema() {
-// verificar se é de "noite", caso seja, colocamos o tema escuro, caso não seja, REMOVEMOS o tema escuro
-    hora >= 18 || hora <= 6
-    ? document.body.classList.add("tema--escuro")
-    : document.body.classList.remove("tema--escuro");
-}
-
-// 
-
-setInterval(atualizarTema, 60000)
